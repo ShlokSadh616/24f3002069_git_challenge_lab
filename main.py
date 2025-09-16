84b9683220640b16dbce37562a9dc4141f6b3ab4
Shlok Sadh <s.shlok2006@gmail.com>
2025-09-16 21:31:40 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from sum import add
+from difference import subtract
+from product import multiply
+from quotient import divide
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("Sum:", add(a, b))
+    print("Difference:", subtract(a, b))
+    print("Product:", multiply(a, b))
+    print("Quotient:", divide(a, b))
